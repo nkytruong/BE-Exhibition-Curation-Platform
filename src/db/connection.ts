3c51ca5a2a0e471778b26b06b5b0ec46535b30e1
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2025-02-04 21:16:57 +0700

    Installs type definitions for node

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,15 +3,14 @@ import dotenv from "dotenv";
 
 const ENV = process.env.NODE_ENV || "development";
 
-// Load environment variables from the appropriate .env file
 dotenv.config({
-  // path: `${__dirname}/../.env.${ENV}`,
-  path: `.env.${ENV}`
+  path: `${__dirname}/../.env.${ENV}`,
 });
+// path: `.env.${ENV}`
 
-// console.log(`🔍 Using environment file: .env.${ENV}`);
+console.log(`🔍 Using environment file: .env.${ENV}`);
 console.log(`📌 PGDATABASE:`, process.env.PGDATABASE);
-// console.log(`📌 DATABASE_URL:`, process.env.DATABASE_URL);
+console.log(`📌 DATABASE_URL:`, process.env.DATABASE_URL);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -21,7 +20,7 @@ const config: PoolConfig = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2; // Limit the number of connections in production
+  config.max = 2;
 }
 
 const db = new Pool(config);
