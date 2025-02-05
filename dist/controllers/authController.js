d6bf93029a9d75bee69ab9383832f3a442f5ea77
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2025-02-05 10:54:13 +0700

    Adds dist folder to .gitignore, removes from Git tracking

--- dist/controllers/authController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logoutUser = exports.authenticateUser = exports.registerUser = void 0;
-const registerUser = (req, res) => { };
-exports.registerUser = registerUser;
-const authenticateUser = (req, res) => { };
-exports.authenticateUser = authenticateUser;
-const logoutUser = (req, res) => { };
-exports.logoutUser = logoutUser;
--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    setupFiles: ["dotenv/config"], // This ensures `.env` files are loaded
-    //   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Additional setup (optional)
-};
-exports.default = config;
