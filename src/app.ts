6d97769363060d8155606d829ee6608f76d1b9dd
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2025-02-07 15:12:31 +0700

    Fixes cors error, adds allowed origin

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+// add frontend url when hosted
+const allowedOrigins = "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
